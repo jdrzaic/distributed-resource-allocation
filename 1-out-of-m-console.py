cf3d344e91d551e91f0de070d886faa3bc6d054f
mjukicbraculj <mjukic-@student.math.hr>
2017-06-02 21:35:12 +0200

    fixing bugs

--- 1-out-of-m-console.py
+++ 1-out-of-m-console.py
@@ -1,13 +1,15 @@
-import sys
-from mpi4py import MPI
-from time import sleep
 from threading import Thread, Lock
+from mpi4py import MPI
+import sys
+from time import sleep
+import commutils
 from enums import CsState, REQUEST, PERMISSION
+from comm import BaseCommAdapter
 
 
-class Process():
-	def __init__(self, comm, logging=False, m=1, info_logging=False):
-		self._comm = comm
+class Process(BaseCommAdapter):
+	def __init__(self, comm, logging=False, m=1, info_logging=True):
+		super().__init__(comm, logging=logging)
 		self._info_logging = info_logging
 		self._numer_of_processes = comm.Get_size()
 		self._m = m
@@ -24,7 +26,7 @@ class Process():
 		self._lock = Lock()
 
 	def start(self):
-		print(self._id, " started...")
+		#print(self._id, " started...")
 		self._communicate_thread.start()
 		if self._info_logging:
 			self._log_thread.start()
@@ -32,52 +34,52 @@ class Process():
 	def _log(self):
 		while True:
 			if self._cs_state == CsState.IN:
-				print('Working...', self._id)
+				commutils.log('Working with resources')
 			elif self._cs_state == CsState.TRYING:
-				print('Trying to get resource...', self._id)
+				commutils.log('Trying ...')
 			else:
-				print('Resting...')
-	        sleep(1)
+				commutils.log('Resting...')
+			sleep(1)
 
 
 	def _communicate(self):
 		while True:
 			if self._comm.Iprobe(source=MPI.ANY_SOURCE, tag=PERMISSION):
-				print(self._id, ": Got permission")
 				with self._lock:
-					permission = comm.recv(source=MPI.ANY_SOURCE, tag=PERMISSION)
+					permission = self._comm.recv(source=MPI.ANY_SOURCE, tag=PERMISSION)
 					self._wait_perm[permission['id']] -= permission['count']
 					if (self._cs_state == CsState.TRYING and self._wait_perm[permission['id']] == 0):
 						self._nb_perm += 1
-
-        	if self._comm.Iprobe(source=MPI.ANY_SOURCE, tag=REQUEST):
-    			print(self._id, ": Got request")
-        		with self._lock:
-        			request = comm.recv(source=MPI.ANY_SOURCE, tag=REQUEST)
-        			self._clock = max(clock_i, request['lrd_i'])
-        			prio = self._cs_state == CsState.IN or (self._cs_state == CsState.TRYING and (self._lrd < request['lrd'] or lrd == request['lrd'] and self._id < request['id']))
-
-        			if prio:
-        				self._perm_delayed[request['id']] += 1
-        			else:
-        				permission = {'id': self._id, 'count': 1}
-        				self._comm.isend(permission, dest=request['id'], TAG=PERMISSION)
+						#print(self._id, ": Got permission from", permission['id'], self._wait_perm, self._nb_perm)
+			if self._comm.Iprobe(source=MPI.ANY_SOURCE, tag=REQUEST):
+				with self._lock:
+					request = self._comm.recv(source=MPI.ANY_SOURCE, tag=REQUEST)
+					#print(self._id, " :Got request from ", request['id'])
+					self._clock = max(self._clock, request['lrd'])
+					prio = (self._cs_state == CsState.IN or (self._cs_state == CsState.TRYING and (self._lrd < request['lrd'] or self._lrd == request['lrd'] and self._id < request['id'])))
+					if prio:
+						self._perm_delayed[request['id']] += 1
+						#print(self._id, " :Request from", request['id'], " is delayed")
+					else:
+						permission = {'id': self._id, 'count': 1}
+						#print(self._id, " :Sending permission to ", request['id'])
+						perm = self._comm.isend(permission, dest=request['id'], tag=PERMISSION)
 
 
 	def release_resource(self):
 		with self._lock:
-			print(self._id, " :Release_resource..")
+			#print(self._id, " :Release_resource..")
 			self._cs_state = CsState.OUT
 			for i in range(self._numer_of_processes):
 				if self._perm_delayed[i] is not 0:
 					permission = {'id': self._id, 'count': 1}
-					self._comm.isend(permission, dest=i, TAG=PERMISSION)
+					self._comm.isend(permission, dest=i, tag=PERMISSION)
 					self._perm_delayed[i] = 0
 
 
 	def acquire_resource(self):
 		with self._lock:
-			print(self._id, ": Acquire_resource...")
+			#print(self._id, " :Acquire_resource...")
 			self._cs_state = CsState.TRYING
 			self._lrd = self._clock + 1
 			self._nb_perm = 0
@@ -89,17 +91,17 @@ class Process():
 
 		while True:
 			with self._lock:
-				if self._nb_perm >= self._numer_of_processes - self._m:
+				if self._nb_perm >= (self._numer_of_processes - self._m):
+					#print(self._id, ": Got RESOURCE...")
 					self._cs_state = CsState.IN
-					print(self._id, ": Got resource...")
 					break
+
 
 	def get_id(self):
 		return self._id
 
-
-def main():
-	process = Process(MPI.COMM_WORLD, m=3)
+def main(argv):
+	process = Process(MPI.COMM_WORLD, logging=False, m=int(argv[1]), info_logging=True)
 	process.start()
 
 	if process.get_id()%3 == 0:
@@ -134,4 +136,4 @@ def main():
 
 
 if __name__ == "__main__":
-    main()
+    main(sys.argv)
